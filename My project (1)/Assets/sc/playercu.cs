e9807ce186a4968251d78c92a7b384fa31c1f5bd
yunseoEhd <yunseo9121@gmail.com>
2026-09-11 12:19:04 +0900

    2025280062이윤서_2주차

---
+++ My project (1)/Assets/sc/playercu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Vector2 moveInput;
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * moveInput.x * moveSpeed * Time.deltaTime);
+    }
+}
+
+
